29849d01d6f622b9173e428922d9204f2c4a2f6b
kyamax <kisho.0125@gmail.com>
2023-01-12 22:43:18 +0900

    Update index.blade.php

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,11 +4,15 @@
 @section('content')
 
 <div class="todo-form">
-  <form action="{{ route('tasks.store') }}" method="POST">
-    @csrf
-    <input id="title" class="task_text" type="text" name="text" value="{{ old('text') }}">
-    <button type="submit">追加</button>
-  </form>
+  
+
+  @foreach($tasks as $task)
+  <div class="all-tasks">{{ $task->title }}</div>
+  <div class="all-tasks">{{ $task->text }}</div>
+  <div class="all-tasks">{{ $task->importance->name }}</div>
+  <div class="all-tasks"></div>
+
+  @endforeach
 </div>
 
 @endsection
